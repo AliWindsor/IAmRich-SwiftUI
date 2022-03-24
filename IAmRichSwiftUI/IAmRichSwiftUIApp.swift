11e0fd1cfcb79e60bb35180b5508c8ec96f7e8a7
Alicia W <aw6@realvnc.com>
2022-03-24 11:01:21 +0000

    Initial Commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Alicia Windsor on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
